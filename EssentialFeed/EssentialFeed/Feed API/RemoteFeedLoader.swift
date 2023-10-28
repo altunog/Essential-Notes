e2db24847eee0d545961290f10427950bbc774e9
Oğuz Kaan Altun <altunog@gmail.com>
2023-10-28 15:02:17 +0300

    move `RemoteFeedLoader` components to production

---
+++ EssentialFeed/EssentialFeed/Feed API/RemoteFeedLoader.swift
@@ -0,0 +1,26 @@
+//
+//  RemoteFeedLoader.swift
+//  EssentialFeed
+//
+//  Created by Oğuz Kaan Altun on 28.10.2023.
+//
+
+import Foundation
+
+public protocol HTTPClient {
+    func get(from url: URL)
+}
+
+public final class RemoteFeedLoader {
+    private let url: URL
+    private let client: HTTPClient
+    
+    public init(url: URL, client: HTTPClient) {
+        self.url = url
+        self.client = client
+    }
+    
+    public func load() {
+        client.get(from: url)
+    }
+}
