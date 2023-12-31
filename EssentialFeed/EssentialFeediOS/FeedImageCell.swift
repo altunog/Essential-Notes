e60ab2bd01bb0247d480f92850453aa8c1bd4f1e
Oğuz Kaan Altun <altunog@gmail.com>
2023-12-31 15:08:28 +0300

    Render loaded feed on successful load completion

---
+++ EssentialFeed/EssentialFeediOS/FeedImageCell.swift
@@ -0,0 +1,14 @@
+//
+//  FeedImageCell.swift
+//  EssentialFeediOS
+//
+//  Created by Oğuz Kaan Altun on 31.12.2023.
+//
+
+import UIKit
+
+public final class FeedImageCell: UITableViewCell {
+    public let locationContainer = UIView()
+    public let locationLabel = UILabel()
+    public let descriptionLabel = UILabel()
+}
