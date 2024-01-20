b1631cfd66b65a99bcb7a5ea0640602e7aca7e8b
Oğuz Kaan Altun <altunog@gmail.com>
2024-01-20 14:25:34 +0300

    Move `ErrorView` to separate file

---
+++ EssentialFeed/EssentialFeediOS/Feed UI/Views/ErrorView.swift
@@ -0,0 +1,12 @@
+//
+//  ErrorView.swift
+//  EssentialFeediOS
+//
+//  Created by Oğuz Kaan Altun on 20.01.2024.
+//
+
+import UIKit
+
+public final class ErrorView: UIView {
+    public var message: String?
+}
