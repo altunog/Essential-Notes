9a0dbc9fb0ece22a1a7acd9be62dae0b17453ebe
Oğuz Kaan Altun <altunog@gmail.com>
2024-01-21 18:09:41 +0300

    Move `FeedLoadingViewModel` to a separate file

---
+++ EssentialFeed/EssentialFeed/Feed Presentation/FeedLoadingViewModel.swift
@@ -0,0 +1,10 @@
+//
+//  FeedLoadingViewModel.swift
+//  EssentialFeed
+//
+//  Created by Oğuz Kaan Altun on 21.01.2024.
+//
+
+public struct FeedLoadingViewModel {
+    public let isLoading: Bool
+}
