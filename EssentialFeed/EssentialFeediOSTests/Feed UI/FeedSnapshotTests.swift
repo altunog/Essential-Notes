b3378d43470d01aacd8eb1db2cc35846faa436f1
Oğuz Kaan Altun <altunog@gmail.com>
2024-06-29 23:34:47 +0300

    Replace protocol composition in a single type with a struct composition to prevent forcing clients to implement methods they don't care about

--- EssentialFeed/EssentialFeediOSTests/Feed UI/FeedSnapshotTests.swift
+++ EssentialFeed/EssentialFeediOSTests/Feed UI/FeedSnapshotTests.swift
@@ -101,12 +101,12 @@ private class ImageStub: FeedImageCellControllerDelegate {
 private extension ListViewController {
     
     func display(_ stubs: [ImageStub]) {
-        let cells: [FeedImageCellController] = stubs.map { stub in
+        let cells: [CellController] = stubs.map { stub in
             let cellController = FeedImageCellController(
                 viewModel: stub.viewModel,
                 delegate: stub)
             stub.controller = cellController
-            return cellController
+            return CellController(cellController)
         }
         
         display(cells)
