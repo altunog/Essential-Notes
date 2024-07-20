846abb75c5195f0bc49a85ab6ca58755afe70493
Oğuz Kaan Altun <altunog@gmail.com>
2024-07-20 09:01:04 +0300

    Update snapshots with iPhone 13

--- EssentialFeed/EssentialFeediOSTests/Helpers/XCTestCase+Snapshot.swift
+++ EssentialFeed/EssentialFeediOSTests/Helpers/XCTestCase+Snapshot.swift
@@ -10,22 +10,22 @@ import XCTest
 extension XCTestCase {
     
     func assert(snapshot: UIImage, named name: String, file: StaticString = #filePath, line: UInt = #line) {
-//        let snapshotURL = makeSnapshotURL(named: name, file: file)
-//        let snapshotData = makeSnapshotData(for: snapshot, file: file, line: line)
-//        
-//        guard let storedSnapshotData = try? Data(contentsOf: snapshotURL) else {
-//            XCTFail("Failed to load stored snapshot at URL: \(snapshotURL). Use the `record` method to store a snapshot before asserting.", file: file, line: line)
-//            return
-//        }
-//        
-//        if snapshotData != storedSnapshotData {
-//            let temporarySnapshotURL = URL(filePath: NSTemporaryDirectory(), directoryHint: .isDirectory)
-//                .appending(path: snapshotURL.lastPathComponent)
-//            
-//            try? snapshotData?.write(to: temporarySnapshotURL)
-//            
-//            XCTFail("New snapshot does not match stored snapshot. New snapshot URL: \(temporarySnapshotURL), Stored snapshot URL: \(snapshotURL)", file: file, line: line)
-//        }
+        let snapshotURL = makeSnapshotURL(named: name, file: file)
+        let snapshotData = makeSnapshotData(for: snapshot, file: file, line: line)
+        
+        guard let storedSnapshotData = try? Data(contentsOf: snapshotURL) else {
+            XCTFail("Failed to load stored snapshot at URL: \(snapshotURL). Use the `record` method to store a snapshot before asserting.", file: file, line: line)
+            return
+        }
+        
+        if snapshotData != storedSnapshotData {
+            let temporarySnapshotURL = URL(filePath: NSTemporaryDirectory(), directoryHint: .isDirectory)
+                .appending(path: snapshotURL.lastPathComponent)
+            
+            try? snapshotData?.write(to: temporarySnapshotURL)
+            
+            XCTFail("New snapshot does not match stored snapshot. New snapshot URL: \(temporarySnapshotURL), Stored snapshot URL: \(snapshotURL)", file: file, line: line)
+        }
     }
     
     func record(snapshot: UIImage, named name: String, file: StaticString = #filePath, line: UInt = #line) {
