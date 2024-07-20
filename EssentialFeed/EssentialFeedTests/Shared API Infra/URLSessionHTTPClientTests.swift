51ea0d042e05aa9be7f085fcef9f4eca1d16d876
Oğuz Kaan Altun <altunog@gmail.com>
2024-07-20 10:36:29 +0300

    Wait for URLProtocol to start loading requests before cancelling the URLSessionTask when running tests to achieve predictable test result and prevent tasks leaking outside the test scope (which could affect other tests).

--- EssentialFeed/EssentialFeedTests/Shared API Infra/URLSessionHTTPClientTests.swift
+++ EssentialFeed/EssentialFeedTests/Shared API Infra/URLSessionHTTPClientTests.swift
@@ -31,11 +31,10 @@ final class URLSessionHTTPClientTests: XCTestCase {
     }
     
     func test_cancelGetFromURLTask_cancelsURLRequest() {
-        let exp = expectation(description: "Wait for request")
-        URLProtocolStub.observeRequest(observer: { _ in exp.fulfill() })
+        var task: HTTPClientTask?
+        URLProtocolStub.onStartLoading { task?.cancel() }
         
-        let receivedError = resultErrorFor(taskHandler: { $0.cancel() }) as? NSError
-        wait(for: [exp], timeout: 1.0)
+        let receivedError = resultErrorFor(taskHandler: { task = $0 }) as? NSError
         
         XCTAssertEqual(receivedError?.code, URLError.cancelled.rawValue)
     }
