b9093f476feb8a1e02eba75dc5e9274b23a83e69
Oğuz Kaan Altun <altunog@gmail.com>
2024-07-27 13:37:50 +0300

    Extract reusable FeedImageDataStoreSpecs

---
+++ EssentialFeed/EssentialFeedTests/Feed Cache/FeedImageDataStoreSpecs/FeedImageDataStoreSpecs.swift
@@ -0,0 +1,15 @@
+//
+//  FeedImageDataStoreSpecs.swift
+//  EssentialFeedTests
+//
+//  Created by Oğuz Kaan Altun on 27.07.2024.
+//
+
+import Foundation
+
+protocol FeedImageDataStoreSpecs {
+    func test_retrieveImageData_deliversNotFoundWhenEmpty() throws
+    func test_retrieveImageData_deliversNotFoundWhenStoredDataURLDoesNotMatch() throws
+    func test_retrieveImageData_deliversFoundDataWhenThereIsAStoredImageDataMatchingURL() throws
+    func test_retrieveImageData_deliversLastInsertedValue() throws
+}
